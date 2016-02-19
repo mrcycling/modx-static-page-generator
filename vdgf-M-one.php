f4b6aeca77dc71af93dbb44e63a489afeec23a01
Tom <mrcycling@gmail.com>
2016-02-19 16:45:10 +0700

    Create vdgf-M-one.php

---
+++ vdgf-M-one.php
@@ -0,0 +1,32 @@
+<?php
+// Log all available properties of the $resource
+$modx->log(MODX_LOG_LEVEL_ERROR, print_r($resource->toArray(),true) );
+
+// Get the page id
+$rid = $resource->get('id');
+$web_url = $resource->get('uri');
+
+
+// define base file path
+$baseurl = '/PATH/TO/public_html/';
+
+// resource created
+// determine if folders exist and create if not    
+$path_parts = pathinfo($web_url);
+$target_path = $path_parts['dirname'];
+          
+if (!file_exists($target_path)) {
+    mkdir($target_path, 0755, true);
+}
+      		
+// get the webpage from MODX
+$contents = file_get_contents('http://URL-TO-MODX-INSTALL/index.php?id=' . $rid);
+         
+// remove comments
+$contents = preg_replace('/<!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->/Uis', '', $contents);
+      	
+// minify
+$contents = preg_replace('/^\s+|\n|\r|\s+$/m', '', $contents);
+
+// save new copy
+   file_put_contents($baseurl . $web_url, $contents);
